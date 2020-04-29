60c0ec0cb1aad4d4bf198f3d3211b8923cf0d282
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2020-04-29 17:46:18 +0200

    customization

--- ports/esp8266/modules/inisetup.py
+++ ports/esp8266/modules/inisetup.py
@@ -52,13 +52,7 @@ def setup():
         f.write(
             """\
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-import uos, machine
-#uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
-#import webrepl
-#webrepl.start()
 gc.collect()
 """
         )
